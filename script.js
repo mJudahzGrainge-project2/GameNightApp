3f8d8e9caac07865bf2ede1c0ee07ad1dd9c9075
Zachary Grainge <zgrainge0@gmail.com>
2022-05-19 15:39:29 -0400

    connect js / fetchAPI

---
+++ script.js
@@ -0,0 +1,27 @@
+const gameApp = {};
+
+gameApp.clientId = `G3733FStWX`
+gameApp.endPoint = `https://api.boardgameatlas.com/api/search`
+
+gameApp.init = () => {
+    gameApp.getGame()
+}
+
+gameApp.getGame = () => {
+    const atlasUrl = new URL(gameApp.endPoint)
+
+    atlasUrl.search = new URLSearchParams({
+        client_id: gameApp.clientId
+    })
+    fetch(atlasUrl)
+        .then((response) => {
+            return response.json();
+        })
+        .then((jsonData) => {
+            console.log(jsonData);
+        })
+}
+
+
+
+gameApp.init();
