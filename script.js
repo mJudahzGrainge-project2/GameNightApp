245ea7990e5825cd11f5295225db192847beeb79
Zachary Grainge <zgrainge0@gmail.com>
2022-05-20 13:36:04 -0400

    added js filter

--- script.js
+++ script.js
@@ -11,17 +11,33 @@ gameApp.getGame = () => {
     const atlasUrl = new URL(gameApp.endPoint)
 
     atlasUrl.search = new URLSearchParams({
+        limit: 100,
         client_id: gameApp.clientId
     })
     fetch(atlasUrl)
         .then((response) => {
             return response.json();
         })
-        .then((jsonData) => {
-            console.log(jsonData);
-        })
+        .then((data) => {
+            gameApp.displayGames(data.games)
+            // console.log(data.games[1].name)
+        });
 }
 
+const playerAmount = document.querySelector('select[name=players]').value;
+const timeLimit = document.querySelector('select[name=time]').value;
+
+gameApp.displayGames = (gameArray) => {
+    const playerFilter = gameArray.filter(function (games) {
+        return games.max_players >= playerAmount && games.max_playtime <= timeLimit;
+    })
+    console.log(playerFilter);
+};
+
+gameApp.getRandomItem = (array) => {
+    const RandomIndex = Math.floor(Math.random() * array.length)
+    return array[RandomIndex];
+}
 
 
 gameApp.init();
